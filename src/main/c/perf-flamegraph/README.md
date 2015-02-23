3b5398c01505c98be87e93189fc760113e671c81
David Capwell <dcapwell@gmail.com>
2015-02-22 17:43:03 -0800

    added counter programs to show perf off

---
+++ src/main/c/perf-flamegraph/README.md
@@ -0,0 +1,13 @@
+# Dependencies
+
+This code depends on pthread, so setup gcc
+
+## Ubuntu
+
+```bash
+sudo apt-get install build-essential
+```
+
+# Reference
+
+These examples are based off of http://jvns.ca/blog/2014/12/14/fun-with-threads/.
---
+++ src/main/c/perf-flamegraph/locking_counter.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define NUM_THREADS     20
+#define NUM_INCREMENTS  1000000
+#define EXPECTED        NUM_THREADS * NUM_INCREMENTS
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int counter;
+
+void *task(void *tid)
+{
+  for(int i = 0; i < NUM_INCREMENTS; i++) {
+    pthread_mutex_lock(&mutex);
+    counter += 1;
+    pthread_mutex_unlock(&mutex);
+  }
+
+  pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+  pthread_t threads[NUM_THREADS];
+  int thread_args[NUM_THREADS];
+  int rc, i;
+  for(i = 0; i < NUM_THREADS; ++i) {
+    thread_args[i] = i;
+    rc = pthread_create(&threads[i], NULL, task, (void *) &thread_args[i]);
+    assert(0 == rc);
+  }
+
+  for(i = 0; i < NUM_THREADS; ++i) {
+    rc = pthread_join(threads[i], NULL);
+    printf("In main: thread %d is complete\n", i);
+    assert(0 == rc);
+  }
+  printf("Final value of counter is: %d\n", counter);
+  printf("Expected value is: %d\nOff by: %d\n", EXPECTED, EXPECTED - counter);
+  pthread_exit(NULL);
+}
